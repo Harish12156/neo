8ac1e10b328d1e5fb4f80a089d72616157c87c65
harish612343 <harish.clarisco@gmail.com>
2024-08-12 14:12:02 +0530

    FE aug 12 harish

--- src/pages/dashboard-pages/Dashboardmain.js
+++ src/pages/dashboard-pages/Dashboardmain.js
@@ -1,41 +1,7 @@
 import React, { useState } from 'react';
-import Btc from '../../assets/images/dashboard/assets/btc.png';
-import Eth from '../../assets/images/dashboard/assets/eth.png';
-import Sol from '../../assets/images/dashboard/assets/sol.png';
-
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import { LuMoveDownRight, LuMoveUpRight } from 'react-icons/lu';
+import Myassets from "./inner/Myassets";
 import ReactApexChart from 'react-apexcharts';
 
-const assets = [
-    {
-        name: 'Bitcoin',
-        imgSrc: Btc,
-        amount: '0.6542 BTC',
-        value: '$48,021.04',
-        percentageChange: '+2.05%',
-        icon: <LuMoveUpRight />,
-        colorClass: 'fc-g'
-    },
-    {
-        name: 'Ethereum',
-        imgSrc: Eth,
-        amount: '0.6542 ETH',
-        value: '$48,021.04',
-        percentageChange: '+0.05%',
-        icon: <LuMoveDownRight />,
-        colorClass: 'fc-r'
-    },
-    {
-        name: 'Solana',
-        imgSrc: Sol,
-        amount: '0.6542 SOL',
-        value: '$48,021.04',
-        percentageChange: '+2.05%',
-        icon: <LuMoveUpRight />,
-        colorClass: 'fc-g'
-    }
-];
 
 function Dashboardmain() {
     const [chartOptions, setChartOptions] = useState({
@@ -118,32 +84,7 @@ function Dashboardmain() {
 
     return (
         <div>
-            <div className="card dash-card-1 mt-4">
-                <div className="card-body">
-                    <h5 className="dash-head-1 mb-4">My Assets</h5>
-                    <div className='row row-cols-1 row-cols-md-3'>
-                        {assets.map((asset, index) => (
-                            <div className='col' key={index}>
-                                <div className="card dash-card-2">
-                                    <div className="card-body">
-                                        <div className='d-flex gap-2 align-items-center'>
-                                            <LazyLoadImage alt={asset.name} src={asset.imgSrc} className='dash-asset-img-2' />
-                                            <p className='mb-0 dash-assets-txt-1'>{asset.name}</p>
-                                            <div className={`ms-auto dash-assets-txt-1 ${asset.colorClass}`}>
-                                                <span>{asset.percentageChange} {asset.icon}</span>
-                                            </div>
-                                        </div>
-                                        <div className='mt-3 d-flex'>
-                                            <h6 className='mb-0'>{asset.amount}</h6>
-                                            <span className='dash-assets-txt-2 ms-auto'>{asset.value}</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+        <Myassets />
 
             <div className="card dash-card-1 mt-4">
                 <div className="card-body">
